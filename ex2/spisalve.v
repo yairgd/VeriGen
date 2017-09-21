c616b4886990b34d8d6fd8a22ca172d4ecbc95d5
Yair Gadelov <yair.gadelov@gmail.com>
2017-09-21 05:45:41 +0300

    spi recv looks good

--- ex2/spisalve.v
+++ ex2/spisalve.v
@@ -1,4 +1,8 @@
-module  spislave (miso,mosi,cs,spi_clk,clk,rst);
+module  spislave # (
+	parameter SPI_WORDLEN = 16 
+)
+(	miso,mosi,cs,spi_clk,clk,rst,rd_value,rd_en,wr_value,wr_en,addr
+);
 input mosi;
 output miso;
 input spi_clk;
@@ -6,7 +10,14 @@ input cs;
 input clk;
 input rst;
 
-parameter SPI_WORDLEN = 16 ;
+input      [7:0] rd_value;
+output reg [7:0] wr_value;
+output [5:0] addr;
+output reg wr_en;
+output reg rd_en;
+
+
+
 
 reg [SPI_WORDLEN-1:0] ser2reg_data;
 reg [SPI_WORDLEN-1:0] ser2reg_data_next;
@@ -15,20 +26,28 @@ reg [6:0] ser2reg_cnt;
 reg [6:0] cnt;
 
 
+//initial ser2reg_data = 0;
+
 reg [1:0] cs_d;
 always @(posedge clk)
 begin:cs_detect
-	cs_d <={cs_d[1],cs};
+	cs_d <={cs_d[0],cs};
 end
 
 reg cs_sync;
 always @(cs_d)
 begin
 	case (cs_d)
-		2'b01: cs_sync=1'b1;
-		2'b10: cs_sync=1'b0;
-		default: cs_sync=cs_sync;
-	endcase
+		2'b01: 
+		begin 
+		cs_sync=1'b1;
+	end
+	2'b10: 
+	begin
+		cs_sync=1'b0;
+	end
+	default: cs_sync=cs_sync;
+endcase
 end
 
 
@@ -36,27 +55,44 @@ end
 
 
 reg [7:0] cmd;
-always  @(posedge spi_clk) 
+always  @(posedge spi_clk or posedge rst) 
 begin:ser2reg
-	if (cs_sync && cnt<SPI_WORDLEN) begin
-		ser2reg_data  <={ser2reg_data[SPI_WORDLEN-2:0],mosi};
-		cnt<=cnt+1;
-		/* 8 MSB bits are command to FPGA*/
-		if (cnt==7) begin
-			cmd<=ser2reg_data[7:0];
-		end
+	if (rst) begin
+		cnt<=0;
+		ser2reg_data<=1'b0;
+	end else begin
+		if (!cs_sync && cnt<SPI_WORDLEN) begin
+			ser2reg_data   <={ser2reg_data[SPI_WORDLEN-2:0], mosi};
+			cnt<=cnt+1;
+			/* 8 MSB bits are command to FPGA*/
+			if (cnt==7) begin
+				cmd<=ser2reg_data[7:0];
+			end
 
+			/* register value */
+			if (cnt==15) begin
+				value<=ser2reg_data[15:8];
+			end
+
+
+		end
 	end
 end
 
 
+reg [7:0] value;
+wire [1:0] opcode = cmd[7:6];
+wire [5:0] addr = cmd[5:0];
 always @(posedge clk)
 begin:cmd_action
-	if (cnt>7) begin
-		case (cmd[7:6])
+	if (cnt>7 && cs_sync == 0) begin
+		case (opcode)
 			2'b01: //read
 			begin
-	
+				rd_en<=1'b1;
+			end
+			2'b10: //write
+			begin
 			end
 		endcase
 	end
--- ex2/tb1.v
+++ ex2/tb1.v
@@ -1,8 +1,8 @@
 module tb1; 
 reg clk, spi_clk,reset, enable; 
 wire [3:0] count; 
-reg [23:0] data_out;
-reg [23:0] data_in;
+reg [SPI_WORDLEN-1:0] data_out;
+reg [SPI_WORDLEN-1:0] data_in;
 reg rst;
 reg cs;
 
@@ -12,21 +12,26 @@ reg [7:0]cnt;
 time delay;
 
 
-localparam SPI_WORDLEN=24;
+localparam SPI_WORDLEN=16;
 
 initial 
 begin
 	$dumpfile("test.vcd");
 	$dumpvars(0,tb1);
-	data_out[23:0]=24'h123456;
+	data_out[15:8] = 8'b01000000;
+	data_out[7:0] = 8'hff;
+
+	reset=1;
+	#5
+	reset=0;
+
+	cs=1;
 	clk = 0;
 	spi_clk = 0;
 	cnt = 0;
-	reset = 0; 
 	enable = 0;
 
-	#700
-	cs =1;
+	#1700
 
 	#5  rst = 0;
 	#5 rst = 1;
@@ -53,7 +58,7 @@ assign mosi = data_out[SPI_WORDLEN-1];
 always @(posedge clk)
 begin:mosi
 	if (cs==1'b0  ) begin  
-		data_out <={data_out[SPI_WORDLEN-2:0], 1'b0};
+		data_out <={data_out[SPI_WORDLEN-2:0] ,1'b0 };
 		cnt<=cnt+1;
 		if (cnt==SPI_WORDLEN+2) begin
 			cs=1;
@@ -67,7 +72,7 @@ end
 
 
 
-spislave #(16) spislave_ins (.miso(),.mosi(data_out[SPI_WORDLEN-1]), .cs(cs), .spi_clk(spi_clk)  ,.clk(clk));
+spislave #(16) spislave_ins (.miso(),.mosi(data_out[SPI_WORDLEN-1]), .cs(cs), .spi_clk(spi_clk)  ,.clk(clk),.rst(reset));
 
 
 endmodule
