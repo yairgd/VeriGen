42e07c0d17794330ef436454afebef246d6a6718
Yair Gadelov <yair.gadelov@gmail.com>
2017-09-23 19:48:37 +0300

    wishbone bus added to spi - not tested

--- ex2/spisalve.v
+++ ex2/spisalve.v
@@ -1,21 +1,48 @@
-module  spislave # (
-	parameter SPI_WORDLEN = 16 
-)
-(	miso,mosi,cs,spi_clk,clk,rst,rd_value,rd_en,wr_value,wr_en,addr
-);
-input mosi;
-output miso;
-input spi_clk;
-input cs;
-input clk;
-input rst;
+`timescale 1ns / 1ps
 
+module  spislave # (
+	parameter SPI_WORDLEN = 16, 
+    	parameter DATA_WIDTH = 8,                // width of data bus in bits (8, 16, 32, or 64)
+    	parameter ADDR_WIDTH = 7,                // width of address bus in bits
+    	parameter SELECT_WIDTH = (DATA_WIDTH/8)  // width of word select bus (1, 2, 4, or 8)
+)
+(
+//	miso,mosi,cs,spi_clk,clk,rst,rd_value,rd_en,wr_value,wr_en,addr
+
+	/* system signals */
+	input clk,
+	input rst,
+
+	/* spi signals */
+	input mosi,
+	output miso,
+	input spi_clk,
+	input cs,
+
+	 /* signals genrtated by this module from master to slavle */
+	output reg  [ADDR_WIDTH-1:0]   wbm_adr_o,    // ADR_O() address output
+	input  wire [DATA_WIDTH-1:0]   wbm_dat_i,    // DAT_I() data in
+	output reg  [DATA_WIDTH-1:0]   wbm_dat_o,    // DAT_O() data out
+	output reg                     wbm_we_o,     // WE_O write enable output
+	output wire [SELECT_WIDTH-1:0] wbm_sel_o,    // SEL_O() select output
+	output reg                     wbm_stb_o,    // STB_O strobe output
+	input  wire                    wbm_ack_i,    // ACK_I acknowledge input
+	input  wire                    wbm_err_i,    // ERR_I error input
+	input  wire                    wbm_rty_i,    // RTY_I retry input
+	output reg                     wbm_cyc_o    // CYC_O cycle output
+
+
+
+);
+
+
+/*
 input      [7:0] rd_value;
 output reg [7:0] wr_value;
 output [5:0] addr;
 output reg wr_en;
 output reg rd_en;
-
+*/
 
 
 
@@ -55,6 +82,7 @@ end
 
 
 reg [7:0] cmd;
+reg wbc_trig;
 always  @(posedge spi_clk or posedge rst) 
 begin:ser2reg
 	if (rst) begin
@@ -71,8 +99,13 @@ begin:ser2reg
 
 			/* register value */
 			if (cnt==15) begin
-				value<=ser2reg_data[15:8];
-			end
+				wbm_dat_o<=ser2reg_data[7:0];
+				wbm_we_o <=cmd[7:7];
+				wbm_adr_o<=cmd[6:0];
+				wbc_trig<=1'b1;
+			end else begin
+				wbc_trig<=1'b0;
+			end	
 
 
 		end
@@ -80,24 +113,41 @@ begin:ser2reg
 end
 
 
-reg [7:0] value;
-wire [1:0] opcode = cmd[7:6];
-wire [5:0] addr = cmd[5:0];
-always @(posedge clk)
-begin:cmd_action
-	if (cnt>7 && cs_sync == 0) begin
-		case (opcode)
-			2'b01: //read
+
+
+localparam  wbc_idle_s=3'd0,wbc_stb_s=3'd1;
+
+reg [2:0] wbc_state;
+always @(posedge clk) 
+begin:wbc_controller
+	if (rst) begin
+		wbm_stb_o<=1'b0;
+		wbc_state<=wbc_idle_s;	
+	end else begin
+		case (wbc_state)
+			wbc_idle_s:
 			begin
-				rd_en<=1'b1;
+				if (wbc_trig) begin
+					wbc_state<=wbc_stb_s;
+					wbm_stb_o<=1'b1;
+					wbm_cyc_o<=1'b1;					
+				end else begin
+					wbc_state<=wbc_idle_s;
+					wbm_stb_o<=1'b0;
+				end
 			end
-			2'b10: //write
+			wbc_stb_s:
 			begin
+				if (wbm_ack_i==1'b0) begin
+					wbc_state<=wbc_stb_s;
+				end else begin
+					wbc_state<=wbc_idle_s;
+					wbm_cyc_o<=1'b0;					
+				end
+
 			end
 		endcase
 	end
+
 end
-
-
-
 endmodule
--- ex2/tb1.v
+++ ex2/tb1.v
@@ -1,9 +1,10 @@
+`timescale 1ns / 1ps
+
 module tb1; 
 reg clk, spi_clk,reset, enable; 
 wire [3:0] count; 
 reg [SPI_WORDLEN-1:0] data_out;
 reg [SPI_WORDLEN-1:0] data_in;
-reg rst;
 reg cs;
 
 
@@ -13,58 +14,67 @@ time delay;
 
 
 localparam SPI_WORDLEN=16;
+localparam ADDR_WIDTH = 7;
+localparam DATA_WIDTH = 8;
+localparam SELECT_WIDTH = 1;
 
 initial 
 begin
 	$dumpfile("test.vcd");
 	$dumpvars(0,tb1);
-	data_out[15:8] = 8'b01000000;
+	data_out[15:8] = 8'b10000001;
 	data_out[7:0] = 8'hff;
 
-	reset=1;
+	reset=0;
 	#5
+	reset=1;
+	#1500
 	reset=0;
 
-	cs=1;
 	clk = 0;
 	spi_clk = 0;
 	cnt = 0;
 	enable = 0;
 
-	#1700
-
-	#5  rst = 0;
-	#5 rst = 1;
-	#5 rst = 0;
-
-	delay = 121; //$dist_uniform(seed, 50, 100);
-	#delay;
-	cs = 0;
 
 end 
 
 
-
+reg [4:0] spi_cnt;
+initial  spi_cnt =0;
 always
 begin
-	#10 clk = ~clk; 
-	#80 spi_clk = ~spi_clk;
+	#10 
+	clk = ~clk;
+	#3
+	if (spi_cnt==8) begin
+		spi_clk=~spi_clk;
+		spi_cnt =0;
+	end else begin
+		spi_cnt=spi_cnt+1;
+	end
+
 end
+
 assign mosi = data_out[SPI_WORDLEN-1];
 
 
 
 
-always @(posedge clk)
+always @(posedge spi_clk or posedge reset)
 begin:mosi
-	if (cs==1'b0  ) begin  
+	if (reset==1) begin
+		cs<=1;
+		cnt<=0;
+		#5000;
+	end else if (cnt<SPI_WORDLEN) begin
 		data_out <={data_out[SPI_WORDLEN-2:0] ,1'b0 };
+		cs<=1'b0;
 		cnt<=cnt+1;
-		if (cnt==SPI_WORDLEN+2) begin
-			cs=1;
-			#5000
-			$finish;
-		end
+	end else begin
+		cs<=1;
+		#5000
+		$finish;
 	end
 end
 
@@ -72,7 +82,24 @@ end
 
 
 
-spislave #(16) spislave_ins (.miso(),.mosi(data_out[SPI_WORDLEN-1]), .cs(cs), .spi_clk(spi_clk)  ,.clk(clk),.rst(reset));
+
+
+wire  [ADDR_WIDTH-1:0]   wbm_adr_o;    // ADR_O() address output
+wire  [DATA_WIDTH-1:0]   wbm_dat_i;    // DAT_I() data in
+wire  [DATA_WIDTH-1:0]   wbm_dat_o;    // DAT_O() data out
+wire 			 wbm_we_o;     // WE_O write enable output
+wire  [SELECT_WIDTH-1:0] wbm_sel_o;    // SEL_O() select output
+wire  		         wbm_stb_o;    // STB_O strobe output
+wire			 wbm_ack_i;    // ACK_I acknowledge input
+wire			 wbm_err_i;    // ERR_I error input
+wire			 wbm_rty_i;    // RTY_I retry input
+wire 			 wbm_cyc_o;    // CYC_O cycle output
+
+spislave #(16) spislave_ins (.miso(),.mosi(data_out[SPI_WORDLEN-1]), .cs(cs), .spi_clk(spi_clk)  ,.clk(clk),.rst(reset),
+	.wbm_adr_o(wbm_adr_o),.wbm_dat_i(wbm_dat_i),.wbm_dat_o(wbm_dat_o),.wbm_we_o(wbm_we_o),.wbm_sel_o(wbm_sel_o),
+	.wbm_ack_i(wbm_ack_i),.wbm_err_i(wbm_err_i),.wbm_rty_i(wbm_rty_i),.wbm_cyc_o(wbm_cyc_o) );
+
+
 
 
 endmodule
